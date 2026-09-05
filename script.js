ce600c647dbc40966df14b31efc45fba4c56f04b
kenny AMOUSSOU <kennyamoussou65@gmail.com>
2026-09-05 03:08:12 +0100

    mise a jour

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+// Sélectionne automatiquement tous les compteurs présents dans le HTML
+// (n'importe quel élément possédant l'attribut data-target)
+const counterElements = document.querySelectorAll('[data-target]');
 
-let count = 2300;
-let target = 2400;
-let prefix = "+";
-let durationInSeconds = 5;
+function animateCounter(element) {
+    const target = parseInt(element.dataset.target, 10);
+    const suffix = element.dataset.suffix || "";
+    // Valeur de départ : si data-start n'est pas défini, on part de 0
+    let count = element.dataset.start ? parseInt(element.dataset.start, 10) : 0;
 
-let steps = target - count;
-let stepTime = (durationInSeconds * 1000)
+    element.innerText = count + suffix;
 
+    const interval = setInterval(() => {
+        count++;
+        element.innerText = count + suffix;
 
-let interval = setInterval( () => {
-    count ++;
-    document.getElementById('counter').innerText = prefix + count;
-    if(count >= target) { clearInterval(interval); }
-}, 30);
+        if (count >= target) {
+            clearInterval(interval);
+        }
+    }, 30);
+}
+
+counterElements.forEach(animateCounter);
